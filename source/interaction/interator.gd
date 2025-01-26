f81343216ba72ed608cf116396fcdf563effb5de
Kaique Silva <128327786+Kaitolinha@users.noreply.github.com>
2025-01-26 02:38:20 -0300

    Fixed Interator signals.

--- source/interaction/interator.gd
+++ source/interaction/interator.gd
@@ -1,26 +1,21 @@
 class_name Interator extends RayCast3D
 
-signal interacted(interactable: Interactable, successful: bool)
 signal focus_entered(interactable: Interactable)
 signal focus_exited(interactable: Interactable)
+signal interacted(interactable: Interactable, successful: bool)
 
 var focus: Interactable
 
 func _process(_delta: float) -> void:
-	# Clear.
-	if not is_colliding():
-		focus_exited.emit(focus)
-		focus = null
-		return
+	var collider: Object = get_collider() if is_colliding() else null
+	var interactable: Interactable = collider if collider is Interactable else null
+	var interactable_exists: bool = is_instance_valid(interactable)
 
-	var collider: Object = get_collider()
+	# Handle signals.
+	if focus != interactable:
+		if is_instance_valid(focus): focus_exited.emit(focus)
+		if interactable_exists: focus_entered.emit(interactable)
+		focus = interactable
 
-	if collider is Interactable:
-		# Handle signals.
-		if focus != collider:
-			if is_instance_valid(focus): focus_exited.emit(focus)
-			focus_entered.emit(collider)
-			focus = collider
-
-		if Input.is_action_just_pressed(&"interact"):
-			interacted.emit(collider, collider.interact(self))
+	if interactable_exists and Input.is_action_just_pressed(&"interact"):
+		interacted.emit(interactable, interactable.interact(self))
