18f9eeaf083155046d018e3ad3837024cfd16581
Kaique Silva <128327786+Kaitolinha@users.noreply.github.com>
2025-02-14 20:20:02 -0300

    Added balancing the player's head when moving.

--- source/player/player.gd
+++ source/player/player.gd
@@ -1,12 +1,16 @@
 class_name Player extends CharacterBody3D
 
-@onready var interator: Interator = $Head/Interator
+@onready var interator: Interator = $Pivot/Head/Interator
 @onready var inventory: Inventory = $Inventory
 var speed: float = 2.5
 var acceleration: float = 7.5
 var controllable: bool = true
 
-@onready var _head: Node3D = $Head
+@onready var _head: Node3D = $Pivot/Head
+var _head_shake_frequency: float = 0.005
+var _head_shake_intensity: float = 0.05
+var _head_shake_acceleration: float = 5.0
+
 static var _mouse_sensitivity: float = 0.001
 static var _camera_threshold: float = deg_to_rad(70.0)
 
@@ -23,6 +27,15 @@ func _physics_process(delta: float) -> void:
 
 	var goal: Vector3 = velocity.lerp(movement * speed, acceleration * delta)
 	velocity = Vector3(goal.x, velocity.y, goal.z)
+
+	# Head shaking.
+	var shake: Vector3 = Vector3.ZERO
+
+	if is_on_floor():
+		var frequency: float = Time.get_ticks_msec() * _head_shake_frequency * floor(velocity.length())
+		shake = Vector3(cos(frequency / 2.0) / 2.0, sin(frequency), sin(frequency) / 2.0)
+
+	_head.position = _head.position.lerp(shake * _head_shake_intensity, _head_shake_acceleration * delta)
 
 	# Gravity logic.
 	if not is_on_floor():
