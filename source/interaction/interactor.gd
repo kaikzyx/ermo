84efd9fb2822c3c73c14748a0d1f2d0195b9bbf0
Kaique Silva <128327786+Kaitolinha@users.noreply.github.com>
2025-03-05 15:50:09 -0300

    Updated Godot version to 4.4 and refactored interactor.

--- source/interaction/interactor.gd
+++ source/interaction/interactor.gd
@@ -4,25 +4,24 @@ signal focus_entered(interactable: Interactable)
 signal focus_exited(interactable: Interactable)
 signal interacted(interactable: Interactable, successful: bool)
 
-var focus: Interactable = null
+var _interactable_id: int = 0
 
 func _process(_delta: float) -> void:
 	if not enabled: return
 
-	var collider: Object = get_collider() if is_colliding() else null
-	var interactable: Interactable = collider if collider is Interactable else null
-	var interactable_exists: bool = is_instance_valid(interactable)
+	var collider_id: int = 0
 
-	# If the current focus is no longer valid, emit the exit signal and reset it.
-	if focus and not is_instance_valid(focus):
-		focus_exited.emit(null)
-		focus = null
+	# If there is an interactable being focused.
+	if is_colliding() and get_collider() is Interactable:
+		collider_id = get_collider().get_instance_id()
+
+		if Input.is_action_just_pressed(&"interact"):
+			var interactable: Interactable = instance_from_id(collider_id)
+			interacted.emit(interactable, interactable.interact(self))
 
 	# Handle signals.
-	if focus != interactable:
-		if is_instance_valid(focus): focus_exited.emit(focus)
-		if interactable_exists: focus_entered.emit(interactable)
-		focus = interactable
-
-	if interactable_exists and Input.is_action_just_pressed(&"interact"):
-		interacted.emit(interactable, interactable.interact(self))
+	if _interactable_id != collider_id:
+		if _interactable_id != 0:
+			focus_exited.emit(instance_from_id(_interactable_id) if is_instance_id_valid(_interactable_id) else null)
+		if is_instance_id_valid(collider_id): focus_entered.emit(instance_from_id(collider_id))
+		_interactable_id = collider_id
